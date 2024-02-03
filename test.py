9887aefb29bd9736d703f7977a37ab693a071700
stupidcasey <caseysutton11@gmail.com>
2024-02-03 12:14:39 -0600

    test

---
+++ test.py
@@ -0,0 +1,9 @@
+print("branch test")
+
+for x in range(32):
+    try:
+        uni_string = 'u04' + x 
+                    
+        print(u\uni_string)
+    except:
+        print('could not print number')
